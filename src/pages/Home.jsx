b4e5774505b2403e5e3ebe670d5db20752f8236e
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-01-26 23:59:19 +0530

    Banner

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+
+
+const Home = () => {
+  return (
+    <>
+    <Hero />
+    <FeaturedSection />
+    <Banner />
+    </>
+  )
+}
+
+export default Home
