d67c828685ca9ea3870c7b9d7fa9e660bd7e0c3d
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-02-03 00:55:11 +0530

    Testimonial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
     <Hero />
     <FeaturedSection />
     <Banner />
+    <Testimonial />
     </>
   )
 }
