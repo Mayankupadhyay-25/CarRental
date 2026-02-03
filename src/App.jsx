5cb9367554f726491c685527de763994fc656d09
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-02-04 01:55:24 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBooking from './pages/MyBooking'
+import Footer from './components/Footer'
 
 const App = () => {
 
@@ -23,6 +24,9 @@ const isOwnerPath = useLocation().pathname.startsWith('/owner')
 
 
       </Routes>
+
+      {!isOwnerPath && <Footer />}
+      
     </>
   )
 }
