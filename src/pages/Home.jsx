2c1a912d760ebd4e78b3747f37b773fa7df10b99
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-02-04 01:55:34 +0530

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import FeaturedSection from '../components/FeaturedSection'
-import Banner from '../components/Banner'
-import Testimonial from '../components/Testimonial'
+import React from "react";
+import Hero from "../components/Hero";
+import FeaturedSection from "../components/FeaturedSection";
+import Banner from "../components/Banner";
+import Testimonial from "../components/Testimonial";
+import Newsletter from "../components/Newsletter";
 
 
 const Home = () => {
   return (
     <>
-    <Hero />
-    <FeaturedSection />
-    <Banner />
-    <Testimonial />
+      <Hero />
+      <FeaturedSection />
+      <Banner />
+      <Testimonial />
+      <Newsletter />
+    
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
