ad316ed00067ad86a500fc2911621d73b7b2d35a
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-02-11 23:38:36 +0530

    CarDetails

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import {assets, dummyCarData} from '../assets/assets' 
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { assets, dummyCarData } from "../assets/assets";
 
 const CarDetails = () => {
-
-  const {id} = useParams()
-  const navigate = useNavigate()
-  const [car, setCar] = useState(null)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [car, setCar] = useState(null);
 
   useEffect(() => {
-    setCar(dummyCarData.find(car => car._id === id))
-  },[id])
+    setCar(dummyCarData.find((car) => car._id === id));
+  }, [id]);
 
-
-  return (
-    <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
-      <button onClick={()=>navigate(-1)} className='flex items-center gap-2 text-gray-500 cursor-pointer hover:text-gray-700'>
-        <img src={assets.arrow_icon} alt="" className='rotate-180 opacity-65' />
-        Back to all cars 
+  return car ? (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-16">
+      <button
+        onClick={() => navigate(-1)}
+        className="flex items-center gap-2 text-gray-500 cursor-pointer hover:text-gray-700"
+      >
+        <img src={assets.arrow_icon} alt="" className="rotate-180 opacity-65" />
+        Back to all cars
       </button>
-      {car ? (
-        <div>
-          {/* Car details content here */}
-        </div>
-      ) : <p>Loading...</p>}
-    </div>
-  )
-}
 
-export default CarDetails
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12 ">
+          {/* left : car image & Details    */}
+          <div></div>
+
+          {/* Right : Car booking form  */}
+          <form action=""></form>
+
+      </div>
+    </div>
+  ) : <p>Loading...</p>
+};
+
+export default CarDetails;
