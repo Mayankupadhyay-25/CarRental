f627b6c71088418b734f0b09eb04463b7ddb40ba
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-05-05 00:35:03 +0530

    addCar

--- src/pages/owner/AddCar.jsx
+++ src/pages/owner/AddCar.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Title from "../../components/owner/Title";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const AddCar = () => {
 
-  const currency = import.meta.env.VITE_CURRENCY
+  const {axios, currency} = useAppContext()
 
   const [image, setImage] = useState(null);
   const [car, setCar] = useState({
@@ -18,10 +20,42 @@ const AddCar = () => {
     location: "",
     description: "",
   });
+  const[isLoading, setIsLoading] = useState(false)
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-  };
+    if(isLoading) return null // prevent multiple submissions
+    setIsLoading(true)
+    try{
+      const fromData = new FormData()
+      fromData.append('image', image)
+      fromData.append('carData', JSON.stringify(car))
+     
+      const {data} = await axios.post('/api/owner/add-car', fromData)
+
+      if(data.success){
+        toast.success(data.message)
+        setImage(null)
+        setCar({
+          brand: "",
+          model: "",
+          year: 0,
+          priceperday: 0,
+          transmission: "",
+          fuelType: "",
+          seating_capacity: 0,
+          location: "",
+          description: "",
+        })
+    }else{
+        toast.error(data.message)
+    }
+  }catch (error) {
+       toast.error(error.message)  
+    } finally{
+      setIsLoading(false)
+  }
+}
 
   return (
     <div className="px-4 py-10 md:px-10 flex-1">
@@ -148,7 +182,7 @@ const AddCar = () => {
           <button className="flex items-center gap-2 px-4 py-2.5 mt-4 bg-primary text-white rounded-md
            font-medium w-max cursor-pointer">
             <img src={assets.tick_icon} alt="" />
-            List your car
+           {isLoading ? "Listing..." : " List your car"}
           </button>
       </form>
     </div>
