d2b0584e88c177df61f7664b08651c9768fafb0e
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-05-07 23:42:14 +0530

    AVAILIBILITY

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,22 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { assets, dummyCarData } from "../assets/assets";
+import { assets } from "../assets/assets";
 import Loading from "../components/Loader";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 
 const CarDetails = () => {
   const { id } = useParams();
+  const {cars, axios, pickupDate, setPickupData: setPickupDate, returnDate, setReturnDate, token, setShowLogin} = useAppContext()
   const navigate = useNavigate();
   const [car, setCar] = useState(null);
   const currency = import.meta.env.VITE_CURRENCY 
 
-  const handleSubmit =async() => {
-    e.preventDefault(e); 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (!token) return setShowLogin(true)
+      const { data } = await axios.post('/api/booking/create', {
+        car: car._id, pickupDate, returnDate, location: car.location
+      })
+      if (data.success) {
+        toast.success(data.message)
+        navigate('/my-bookings')
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(() => {
-    setCar(dummyCarData.find((car) => car._id === id));
-  }, [id]);
+    setCar(cars.find((c) => c._id === id));
+  }, [cars, id]);
 
   return car ? (
     <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-16">
@@ -95,13 +112,13 @@ const CarDetails = () => {
 
             <div className="flex flex-col gap-2">
               <label htmlFor="pickup-date">Pickup Date</label>
-              <input type="date" className="border border-borderColor px-3 py-2 rounded-lg" required id='pickup-date'
-              min={new Date ().toISOString().split('T')[0]}/>
+              <input type="date" value={pickupDate} onChange={e => setPickupDate(e.target.value)} className="border border-borderColor px-3 py-2 rounded-lg" required id='pickup-date'
+              min={new Date().toISOString().split('T')[0]}/>
             </div>
 
              <div className="flex flex-col gap-2">
               <label htmlFor="return-date">Return Date</label>
-              <input type="date" className="border border-borderColor px-3 py-2 rounded-lg" required id='return-date'/>
+              <input type="date" value={returnDate} onChange={e => setReturnDate(e.target.value)} className="border border-borderColor px-3 py-2 rounded-lg" required id='return-date'/>
             </div>
 
             <button className="w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium
