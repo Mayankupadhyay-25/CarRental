0dee5efea96f6058ebce4022029c69cfcefd6e99
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-05-18 22:24:37 +0530

    NavbarError

--- src/components/owner/NavbarOwner.jsx
+++ src/components/owner/NavbarOwner.jsx
@@ -14,7 +14,7 @@ const NavbarOwner = () => {
         <Link to='/'>
         <img src={assets.logo} alt="" className="h-7" />
         </Link>
-           <p>Welcome, {user?.name}</p>
+           <p>Welcome, {user?.name || 'Owner'}</p>
     </div>
   )
 }
