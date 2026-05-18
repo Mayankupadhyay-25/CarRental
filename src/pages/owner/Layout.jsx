d6794b4a86e8f66f411e6aa75989fffd6a5bff57
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-05-18 22:25:04 +0530

    error

--- src/pages/owner/Layout.jsx
+++ src/pages/owner/Layout.jsx
@@ -5,22 +5,23 @@ import Sidebar from '../../components/owner/Sidebar'
 import { useAppContext } from '../../context/AppContext'
 
 const Layout = () => {
-  const {isOwner, navigate} = useAppContext()
+  const { isOwner, navigate, user, loading } = useAppContext()
 
-  useEffect(()=> {
-    if (!isOwner){
+  useEffect(() => {
+    if (!loading && user !== null && !isOwner) {
       navigate('/')
     }
-    },[isOwner]
-  )
+  }, [isOwner, user, loading])
+
+  if (loading) return null
 
   return (
     <div className='flex flex-col'>
-        <NavbarOwner/>
-        <div className='flex'>
-            <Sidebar/>
-            <Outlet/>
-        </div>
+      <NavbarOwner />
+      <div className='flex'>
+        <Sidebar />
+        <Outlet />
+      </div>
     </div>
   )
 }
