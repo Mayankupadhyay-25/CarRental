3628fd8cd7ddc32794e47831a1d40b9face2355f
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-05-25 15:37:20 +0530

    bug fix

--- src/pages/MyBooking.jsx
+++ src/pages/MyBooking.jsx
@@ -40,7 +40,7 @@ const MyBooking = () => {
           <motion.div
           initial={{opacity: 0, y: 20}}
           animate={{opacity: 1, y:0 }}
-          transition={{delay:index * 0.1,  duration: 0.4}}
+          transition={{delay:index * 0.1,  duration :0.4}}
             key={booking._id}
             className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor
            rounded-lg mt-5 first:mt-12"
